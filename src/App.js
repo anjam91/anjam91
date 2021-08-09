1f916338ad19c09cb04fea7c9e7f3a97dc2e32f8
anjam91 <88676374+anjam91@users.noreply.github.com>
2021-08-09 18:39:55 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+//components
+import ScrollToTop from './components/ScrollToTop';
+import Navigation from './components/Navigation'
+import Weather from './components/Weather/Weather'
+import UserProfile from './components/UserProfile'
+import About from './components/About'
+
+//styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+
+
+const App = () => {
+  
+  return ( 
+    <Router basename={process.env.PUBLIC_URL}> 
+      <ScrollToTop />
+      <>
+          <Navigation />
+
+          <Switch>
+            <Route path='/about'>
+              <About />
+            </Route>
+
+            <Route path='/myprofile'>
+              <UserProfile />
+            </Route>
+
+            <Route path='/'>
+              <Weather />
+            </Route>
+        </Switch>
+
+      </>
+    </Router>
+  )
+ 
+}
+
+export default App;
